ac8dd6346469c2bf266c36c17d4370772b275753
Marius Kirchner <mariuskirchner@hotmail.de>
2025-02-19 16:56:54 +0100

    Added Plotting for standard deviation instead of min/max values

--- AdditionalScripts/PlotScripts/FullPlot.py
+++ AdditionalScripts/PlotScripts/FullPlot.py
@@ -1,3 +1,4 @@
+import math
 import os
 import sys
 
@@ -82,8 +83,44 @@ for x in range(0, len(listOfData[0])):
     plt.plot(xnew, smoothavg, color="b")
     plt.fill_between(xnew, smoothmin, smoothmax, color="cornflowerblue")
     print(newfolderdir)
-    plt.savefig(newfolderdir + "\\" + header[counter+1] + ".png", bbox_inches="tight")
-    plt.savefig(newfolderdir + "\\" + header[counter + 1] + ".svg", bbox_inches="tight")
+    plt.savefig(newfolderdir + "\\" + "Total" + header[counter+1] + ".png", bbox_inches="tight")
+    plt.savefig(newfolderdir + "\\" + "Total" + header[counter + 1] + ".svg", bbox_inches="tight")
+    print("done")
+    counter += 1
+    plt.clf()
+for x in range(0, len(listOfData[0])):
+    maxplot = []
+    minplot = []
+    avgplot = []
+    print(len(listOfData[0][x]))
+    for currx in range(0, minticks):
+        tempcounter = 0
+        avgsum = 0
+        for currsim in range(0, numberOfSims):
+            avgsum += listOfData[currsim][x][currx]
+            tempcounter += 1
+        tempavg = avgsum / tempcounter
+        totaldev = 0
+        for currsim in range(0, numberOfSims):
+            totaldev += (listOfData[currsim][x][currx] - tempavg)**2
+        variance = totaldev / tempcounter
+        standarddev = math.sqrt(variance)
+        maxplot.append(tempavg+standarddev)
+        minplot.append(tempavg-standarddev)
+        avgplot.append(tempavg)
+        print(tempavg)
+    tempsmoothavg = make_interp_spline(xaxis, avgplot, k=3)
+    smoothavg = tempsmoothavg(xnew)
+    tempsmoothmin = make_interp_spline(xaxis, minplot, k=3)
+    smoothmin = tempsmoothmin(xnew)
+    tempsmoothmax = make_interp_spline(xaxis, maxplot, k=3)
+    smoothmax = tempsmoothmax(xnew)
+    print("done interpolating")
+    plt.plot(xnew, smoothavg, color="b")
+    plt.fill_between(xnew, smoothmin, smoothmax, color="cornflowerblue")
+    print(newfolderdir)
+    plt.savefig(newfolderdir + "\\" + "StandardDeviation" + header[counter+1] + ".png", bbox_inches="tight")
+    plt.savefig(newfolderdir + "\\" + "StandardDeviation" + header[counter + 1] + ".svg", bbox_inches="tight")
     print("done")
     counter += 1
     plt.clf()
